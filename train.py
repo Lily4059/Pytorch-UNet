36604712d5be8f7fe75904b07fe51459afbeb766
Lily4059 <zsq_hh@126.com>
2025-11-02 09:10:54 +0800

    1轮144张图片测试

--- train.py
+++ train.py
@@ -169,7 +169,7 @@ def train_model(
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=2, help='Number of epochs')    #修改了训练轮次
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=1, help='Number of epochs')    #修改了训练轮次
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
                         help='Learning rate', dest='lr')
